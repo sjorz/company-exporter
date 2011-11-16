bcbe24f136d89c7e4fce25138368d4b1a8e7705a
Rent User <rent@debbie.rent.com.au>
2011-11-16 16:22:10 +0800

    Add odbc_test.staging.php and table.staging.php

---
+++ odbc_test.staging.php
@@ -0,0 +1,41 @@
+<?php
+
+//**********************************************************************
+//
+//  ODBC Connection tester. Connects to server defined by $dsn (see
+//  /etc/odbc/ini). Username $user, password $pwd.
+//
+//  History:
+//  -------
+//  1.0     20Jul11 GB      Original
+//
+//**********************************************************************
+
+  //$dsn = "Rent_Production";
+  //$user = "rentLive";
+  //$pwd = "Consult23";
+
+  //$dsn = "RentTst";
+  //$user = "sa";
+  //$pwd = "abc123";
+
+  $dsn = "Rent_Staging";
+  $user = "rent_staging";
+  $pwd = "GreenFish123";
+
+						 
+  printf ("Connecting to [%s] as [%s][%s]...\n", $dsn, $user, $pwd);
+
+  if (!$connection = odbc_connect ($dsn, $user, $pwd))
+  {
+    echo "Cannot connect\n";
+    exit;
+  }
+  else
+  {
+    echo "Connected to rent_staging\n";
+  }
+  odbc_close ($connection);
+  echo "Done";
+
+?>
