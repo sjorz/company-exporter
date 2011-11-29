030b8a884716f7949ceacaf9e5fd3621fb157d0f
Rent User <rent@debbie.rent.com.au>
2011-11-29 14:40:03 +0800

    Remove exit statement from error function in table.php. Was a cause of many headaches

--- table.prod.php
+++ table.prod.php
@@ -73,7 +73,6 @@ class Table
     private function error ($msg)
     {
         Logger::logError ($msg);
-        exit ($this->__toString() . ': ' . $msg);
     }
 
     //**********************************************************************
